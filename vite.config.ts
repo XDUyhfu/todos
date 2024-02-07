8e57d82aeb92c57916f4908e1825be4706cd34a7
fuyuanhong <fuyuanhong@bytedance.com>
2024-02-07 23:54:50 +0800

    feat: 构建发布

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 将根路径换成相对路径
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
     css: true,
   },
-})
+});
